58d6cdde60f4c32edb0efcd801dafa6a78478b7a
Spiridonov <annie.skywalker@yandex.ru>
2024-12-02 15:01:35 +0300

    base functional

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
-
+    <button @click='addLiks'>like</button>
+    <button @click='addDislike'>dislike</button>
+    <div>
+        <div>{{ like }}</div>
+        <div>{{ dislike }}</div>
+    </div>
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+            like: 0,
+            dislike: 0,
+        }
+    },
+    methods: {
+        addLiks () {
+            this.like += 1;
+
+        },
+        addDislike () {
+            this.dislike += 1;
+        }
+
+    }
+}
+
+</script>
 
 <style>
 
