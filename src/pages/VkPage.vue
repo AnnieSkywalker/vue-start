3c98085a65532ea912a554dd00616175a54034b2
spiridonov <annie.skywalker@yandex.ru>
2024-12-19 01:17:12 +0300

    jsonp

--- src/pages/VkPage.vue
+++ src/pages/VkPage.vue
@@ -24,8 +24,135 @@
                 </li>
             </ul>
         </my-section>
+        <my-button @click='ViewFriends' class='open'>Посмотреть друзей</my-button>
+        <my-section v-if='!isFriendsLoading' class='friends'>
+            <!-- <ul class='friends__list'>
+                <li class='friends__item'>
+                    <span class='friends__id rotate'>{{ userData.user_id }}</span>
+                    <div class='friends__container'>
+                        <img class='friends__image' :src='userData.avatar' alt="imag">
+                        <div class='friends__full-name-block'>
+                            <span class='friends__first-name' >
+                                {{ userData.first_name }}
+                            </span>
+                            <span class='friends__last-name'>
+                                {{userData.last_name}}
+                            </span>
+                        </div>
+                        <div class='friends__button'>
+                            <my-button class='open'>добавить</my-button>
+                            <my-button class='del'>удалить</my-button>
+                        </div>
+                    </div>
+                </li>
+            </ul> -->
+        </my-section>
+        <my-loader v-else-if="isFriendsLoading"></my-loader>
     </div>
 </template>
+
+
+<script>
+import axios from 'axios';
+import * as VKID from '@vkid/sdk';
+import { jsonp } from 'vue-jsonp'
+
+
+    export default {
+        name: 'VkPage',
+        data () {
+            return {
+                client_Id: 52858991,
+                userData: '',
+                isFriendsLoading: false,
+                accessToken: '',
+                idToken: '',
+                fields: 'bdate',
+                vkidVersion: 5.199,
+
+            }
+        },
+        methods: {
+            async ViewFriends () {
+                this.isFriendsLoading = true;
+
+                try {
+                    const result = await jsonp('https://api.vk.com/method/friends.get', {
+                        access_token: this.accessToken,
+                        user_ids: this.userData.user_id,
+                        v: this.vkidVersion,
+                        fields: this.fields,
+                    })
+
+
+                        console.log(result)
+
+
+                } catch(e) {
+                    console.log(e)
+                }
+                this.isFriendsLoading = false;
+
+                //     this.isFriendsLoading = true;
+
+                //     let response = await axios.get('https://api.vk.com/method/status.get', {
+                //         params: {
+                //             access_token: this.accessToken,
+                //             user_ids: this.userData.user_id,
+                //             v: this.vkidVersion
+                //     //     fields: this.fields,
+                //          }
+                //     })
+                //     console.log(response)
+                //     // https://api.vk.com/method/status.get?user_id=743784474&v=5.131&access_token=vk1.a.8mo8TRf0jm67Nla3W3fFbe9qKhMNNMqg21DQLgaPUj..
+
+                // } catch(e) {
+                //     console.log(e)
+                // }
+                // this.isFriendsLoading = false;
+            }
+
+        },
+        mounted () {
+            console.log(VKID)
+            VKID.Config.set({
+                app: 52858991, 
+                redirectUrl: `http://localhost/page-vk`,
+                scope: 'email phone',
+                responseMode: VKID.ConfigResponseMode.Callback,
+            })
+            const oneTap = new VKID.OneTap();
+            const container = document.getElementById('VkIdSdkOneTap');
+
+            if (container) {
+            // Отрисовка кнопки в контейнере с именем приложения APP_NAME, светлой темой и на русском языке.
+                oneTap.render({ container: container, scheme: VKID.Scheme.LIGHT, lang: VKID.Languages.RUS })
+                .on(VKID.WidgetEvents.ERROR, e => console.log(e))
+                .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS,  (payload) => {
+                    console.log(payload)
+                    const code = payload.code;
+                    const deviceId = payload.device_id;
+                    VKID.Auth.exchangeCode(code, deviceId)
+                    .then(data => {
+                        if (data && data.access_token) {
+                            this.accessToken =data.access_token;
+                            this.idToken = data.id_token;
+                            VKID.Auth.userInfo(this.accessToken)
+                                .then((response) => {
+                                    this.userData = response.user;
+                                    console.log(this.userData);
+                                })
+                                .catch(e => console.log(e))
+                        } else {
+                            console.error('Неожиданные данные', data);
+                        }
+                    })
+                    .catch(e => console.log(e));
+                });
+            }
+        }
+    }
+</script>
 
 <style lang="css" scoped>
 .friends {
@@ -99,57 +226,3 @@
 }
 
 </style>
-
-
-<script>
-import axios from 'axios';
-import * as VKID from '@vkid/sdk';
-
-
-    export default {
-        name: 'VkPage',
-        data () {
-            return {
-                client_Id: 52858991,
-                userData: ''
-            }
-        },
-        mounted () {
-            VKID.Config.set({
-                app: 52858991, 
-                redirectUrl: `http://localhost/page-vk`,
-                scope: 'email phone',
-                responseMode: VKID.ConfigResponseMode.Callback,
-            })
-            const oneTap = new VKID.OneTap();
-            const container = document.getElementById('VkIdSdkOneTap');
-
-            if (container) {
-            // Отрисовка кнопки в контейнере с именем приложения APP_NAME, светлой темой и на русском языке.
-                oneTap.render({ container: container, scheme: VKID.Scheme.LIGHT, lang: VKID.Languages.RUS })
-                .on(VKID.WidgetEvents.ERROR, e => console.log(e))
-                .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS,  (payload) => {
-                    console.log(payload)
-                    const code = payload.code;
-                    const deviceId = payload.device_id;
-                    VKID.Auth.exchangeCode(code, deviceId)
-                    .then(data => {
-                        if (data && data.access_token) {
-                            const accessToken =data.access_token;
-                            const idToken = data.id_token;
-                            VKID.Auth.userInfo(accessToken)
-                                .then((response) => {
-                                    this.userData = response.user;
-                                    console.log(this.userData);
-                                })
-                                .catch(e => console.log(e))
-                        } else {
-                            console.error('Неожиданные данные', data);
-                        }
-                    })
-                    .catch(e => console.log(e));
-                });
-            }
-        }
-    }
-</script>
