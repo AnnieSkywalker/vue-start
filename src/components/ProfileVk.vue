eff3338817aee517ab1140541b6175a0bd58247c
spiridonov <annie.skywalker@yandex.ru>
2024-12-20 08:15:17 +0300

    added component profile-vk

---
+++ src/components/ProfileVk.vue
@@ -0,0 +1,93 @@
+<template>
+    <section class='profile-vk'>
+        <div class='profile-vk__avatar-block'>
+            <img class='profile-vk__avatar' :src='href' alt="imag">
+        </div>
+        <span class='profile-vk__id'>{{ userId }}</span>
+        <div class='profile-vk__full-name-block'>
+            <span class='profile-vk__first-name' >
+                {{ first_name }} 
+            </span>
+            <span class='profile-vk__last-name'>
+                {{ last_name }}
+            </span>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'profile-vk',
+        props: {
+            href: {
+                type: String,
+            },
+            userId: {
+                type: String,
+            },
+            first_name: {
+                type: String
+            },
+            last_name: {
+                type: String
+            },
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+
+.profile-vk {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+}
+
+@media screen and (max-width: 767px)  {
+    .profile-vk {
+        padding-top: 15px;
+        grid-template-columns: 100%;
+        grid-template-rows: max-content;
+        column-gap: 0;
+        justify-items: center;
+    }
+}
+
+.profile-vk__avatar-block {
+    background-color: transparent;
+    border: none;
+    border-radius: 50%;
+    height: 120px;
+    padding: 0;
+    width: 120px;
+}
+
+.profile-vk__avatar {
+    border-radius: 50%;
+    box-sizing: border-box;
+    height: 120px;
+    -o-object-fit: cover;
+    object-fit: cover;
+    position: relative;
+    width: 120px;
+}
+
+.profile-vk__full-name-block {
+    display: flex;
+    gap: 10px;
+}
+
+.profile-vk__first-name, .profile-vk__last-name {
+    text-align: start;
+    width: 60%;
+    font-size: 24px;
+    color: var(--text-title);
+}
+.profile-vk__id {
+    font-size: 11px;
+    line-height: 150%;
+    color: var(--text);
+}
+
+</style>
--- src/pages/VkPage.vue
+++ src/pages/VkPage.vue
@@ -1,19 +1,23 @@
 <template>
     <div>
-        <div id="VkIdSdkOneTap"></div>
+        <my-button @click='logout' class='del'>выйти</my-button>
+        <div v-if='!isAuth' id="VkIdSdkOneTap"></div>
+        
         <br/>
-        <my-section v-if='userData' class='friends'>
+        <profile-vk v-if='isAuth' :href='userData.avatar' :userId='userData.user_id' :last_name='userData.last_name' :first_name='userData.first_name '></profile-vk>
+        
+        <my-section v-if='!isFriendsLoading' class='friends'>
             <ul class='friends__list'>
-                <li class='friends__item'>
-                    <span class='friends__id rotate'>{{ userData.user_id }}</span>
+                <li v-for='user in friendsUser' class='friends__item'>
+                    <span class='friends__id rotate'>{{ user.user_id }}</span>
                     <div class='friends__container'>
-                        <img class='friends__image' :src='userData.avatar' alt="imag">
+                        <img class='friends__image' :src='user.photo_200_orig' alt="imag">
                         <div class='friends__full-name-block'>
                             <span class='friends__first-name' >
-                                {{ userData.first_name }}
+                                {{ user.first_name }}
                             </span>
-                            <span class='friends__last-name'>
-                                {{userData.last_name}}
+                            <span class='fravatariends__last-name'>
+                                {{user.last_name}}
                             </span>
                         </div>
                         <div class='friends__button'>
@@ -24,58 +28,67 @@
                 </li>
             </ul>
         </my-section>
-        <my-button @click='ViewFriends' class='open'>Посмотреть друзей</my-button>
-        <my-section v-if='!isFriendsLoading' class='friends'>
-            <!-- <ul class='friends__list'>
-                <li class='friends__item'>
-                    <span class='friends__id rotate'>{{ userData.user_id }}</span>
-                    <div class='friends__container'>
-                        <img class='friends__image' :src='userData.avatar' alt="imag">
-                        <div class='friends__full-name-block'>
-                            <span class='friends__first-name' >
-                                {{ userData.first_name }}
-                            </span>
-                            <span class='friends__last-name'>
-                                {{userData.last_name}}
-                            </span>
-                        </div>
-                        <div class='friends__button'>
-                            <my-button class='open'>добавить</my-button>
-                            <my-button class='del'>удалить</my-button>
-                        </div>
-                    </div>
-                </li>
-            </ul> -->
-        </my-section>
         <my-loader v-else-if="isFriendsLoading"></my-loader>
     </div>
 </template>
-
 
 <script>
 import axios from 'axios';
 import * as VKID from '@vkid/sdk';
 import { jsonp } from 'vue-jsonp'
-
+import ProfileVk from '../components/ProfileVk.vue'
 
     export default {
         name: 'VkPage',
+        components: {
+            ProfileVk
+        },
         data () {
             return {
+                isAuth: false,
                 client_Id: 52858991,
                 userData: '',
                 isFriendsLoading: false,
                 accessToken: '',
                 idToken: '',
-                fields: 'bdate',
+                friendsUser: [],
+                fields: 'photo_200_orig',
                 vkidVersion: 5.199,
-
             }
         },
         methods: {
+            logout () {
+                VKID.Auth.logout(this.accessToken)
+                    .then((data) => {
+                        if(data.response == 1) {
+                            this.isAuth = false;
+                            this.accessToken = '',
+                            this.idToken = '',
+                            this.userData = ''
+                        }
+                    })
+                    .catch(e => console.log('logout тут такая сякая ошибка' + e));
+            },
+            getUser (response) {
+                console.log(response)
+                this.userData = response.user;
+                this.isAuth = true;
+                console.log(this.userData);
+                this.ViewFriends();
+            },
+            receivingAccess (data) {
+                if (data && data.access_token) {
+                    this.accessToken =data.access_token;
+                    this.idToken = data.id_token;
+                    VKID.Auth.userInfo(this.accessToken)
+                        .then(this.getUser)
+                        .catch(e => console.log(e))
+                } else {
+                    console.error('Неожиданные данные', data);
+                }
+            },
             async ViewFriends () {
                 this.isFriendsLoading = true;
-
                 try {
                     const result = await jsonp('https://api.vk.com/method/friends.get', {
                         access_token: this.accessToken,
@@ -83,70 +96,32 @@ import { jsonp } from 'vue-jsonp'
                         v: this.vkidVersion,
                         fields: this.fields,
                     })
-
-
                         console.log(result)
-
-
+                        this.friendsUser = result.response.items;
                 } catch(e) {
                     console.log(e)
                 }
                 this.isFriendsLoading = false;
-
-                //     this.isFriendsLoading = true;
-
-                //     let response = await axios.get('https://api.vk.com/method/status.get', {
-                //         params: {
-                //             access_token: this.accessToken,
-                //             user_ids: this.userData.user_id,
-                //             v: this.vkidVersion
-                //     //     fields: this.fields,
-                //          }
-                //     })
-                //     console.log(response)
-                //     // https://api.vk.com/method/status.get?user_id=743784474&v=5.131&access_token=vk1.a.8mo8TRf0jm67Nla3W3fFbe9qKhMNNMqg21DQLgaPUj..
-
-                // } catch(e) {
-                //     console.log(e)
-                // }
-                // this.isFriendsLoading = false;
             }
-
         },
         mounted () {
-            console.log(VKID)
             VKID.Config.set({
                 app: 52858991, 
-                redirectUrl: `http://localhost/page-vk`,
-                scope: 'email phone',
+                redirectUrl: (window.location.host === 'localhost') ? `http://localhost/page-vk` : 'https://spirridonovka-pet-project.ru/',
                 responseMode: VKID.ConfigResponseMode.Callback,
             })
             const oneTap = new VKID.OneTap();
             const container = document.getElementById('VkIdSdkOneTap');
 
             if (container) {
-            // Отрисовка кнопки в контейнере с именем приложения APP_NAME, светлой темой и на русском языке.
-                oneTap.render({ container: container, scheme: VKID.Scheme.LIGHT, lang: VKID.Languages.RUS })
+            oneTap.render({ container: container, scheme: VKID.Scheme.DARK, lang: VKID.Languages.RUS })
                 .on(VKID.WidgetEvents.ERROR, e => console.log(e))
                 .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS,  (payload) => {
                     console.log(payload)
                     const code = payload.code;
                     const deviceId = payload.device_id;
                     VKID.Auth.exchangeCode(code, deviceId)
-                    .then(data => {
-                        if (data && data.access_token) {
-                            this.accessToken =data.access_token;
-                            this.idToken = data.id_token;
-                            VKID.Auth.userInfo(this.accessToken)
-                                .then((response) => {
-                                    this.userData = response.user;
-                                    console.log(this.userData);
-                                })
-                                .catch(e => console.log(e))
-                        } else {
-                            console.error('Неожиданные данные', data);
-                        }
-                    })
+                    .then(this.receivingAccess)
                     .catch(e => console.log(e));
                 });
             }
@@ -163,14 +138,23 @@ import { jsonp } from 'vue-jsonp'
 .friends__list {
     width: 100%;
     display: flex;
+    flex-direction: column;
+    gap: 20px;
     margin: 0;
     padding: 0;
 }
 
 .friends__item {
     width: 100%;
+    padding: 10px;
     display: flex;
     flex-direction: row;
+    border-bottom: 1px solid var(--border-color);
+    cursor: pointer;
+}
+
+.friends__item:hover {
+    opacity: 0.6;
 }
 
 .friends__id {
