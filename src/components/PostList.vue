9ce102a834820a6059d38f0fce584d96666e6c04
spiridonov <annie.skywalker@yandex.ru>
2024-12-27 23:13:37 +0300

    composition vue

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,52 +1,38 @@
 <template>
-    <section class="posts" >
-        <div v-if='posts.length > 0'>
-            <h2 class="section-title">
-            Список постов
-            </h2>
-            <TransitionGroup name='post-list'>
-                <PostsItem 
-                    v-for='(post, index) in posts'
-                    :post='post'
-                    :index='index'
-                    :key='post.id'
+    <section class="posts">
+        <div v-if="posts.length > 0">
+            <h2 class="section-title">Список постов</h2>
+            <TransitionGroup name="post-list">
+                <PostsItem
+                    v-for="(post, index) in posts"
+                    :post="post"
+                    :index="index"
+                    :key="post.id"
                     @remove="$emit('remove', post)"
                 ></PostsItem>
             </TransitionGroup>
         </div>
-        <h2 v-else class="section-title">
-            Посты не найдены!
-        </h2>
-        
+        <h2 v-else class="section-title">Посты не найдены!</h2>
     </section>
 </template>
 
-<script>
-import PostsItem from './PostItem.vue';
-    
-    export default {
-        components: {
-            PostsItem
-        },
-        props: {
-            posts: {
-                type: Array,
-                required: true
-            }
-        }
-    }
+<script setup>
+    import { defineProps } from 'vue';
+    import PostsItem from './PostItem.vue';
+
+    const { posts } = defineProps({ posts: { type: Array, required: true } });
 </script>
 
 <style lang="css" scoped>
-.post-list-move,
-.post-list-enter-active,
-.post-list-leave-active {
-    transition: all 0.4s ease;
-}
+    .post-list-move,
+    .post-list-enter-active,
+    .post-list-leave-active {
+        transition: all 0.4s ease;
+    }
 
-.post-list-enter-from,
-.post-list-leave-to {
-    opacity: 0;
-    transform: translatex(130px);
-}
+    .post-list-enter-from,
+    .post-list-leave-to {
+        opacity: 0;
+        transform: translatex(130px);
+    }
 </style>
