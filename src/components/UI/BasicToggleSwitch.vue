74f8d5d320a6aab26ee8bc2b6e9096a97254c626
spiridonov <annie.skywalker@yandex.ru>
2024-12-28 22:18:34 +0300

    composition vue, basictoggleswitch

--- src/components/UI/BasicToggleSwitch.vue
+++ src/components/UI/BasicToggleSwitch.vue
@@ -1,97 +1,96 @@
 <template>
     <label class="switch">
-        <input type="checkbox" :checked='modelValue' @click="toggleCheckbox">
+        <input type="checkbox" :checked="modelValue" @click="toggleCheckbox" />
         <div class="slider round"></div>
     </label>
 </template>
-  
-<script>
-export default {
-    props: {
+
+<script setup>
+    import { defineEmits, defineProps } from 'vue';
+
+    const { modelValue } = defineProps({
         modelValue: {
             type: Boolean,
             required: true,
-        }
-    },
-    methods: {
-        toggleCheckbox() {
-            this.$emit('update:modelValue', event.target.checked)
         },
-    },
-}
+    });
+    const emit = defineEmits(['update:modelValue']);
+    const toggleCheckbox = event => {
+        emit('update:modelValue', event.target.checked);
+    };
 </script>
-  
+
 <style scoped>
-.switch {
-    position: relative;
-    display: inline-block;
-    width: 40px;
-    height: 20px;
-    margin: 10px;
-    caret-color: transparent;
-    cursor: none;
-}
+    .switch {
+        position: relative;
+        display: inline-block;
+        width: 40px;
+        height: 20px;
+        margin: 10px;
+        caret-color: transparent;
+        cursor: none;
+    }
 
-.switch::after {
-    content: '';
-    display: block;
-    width: 30px;
-    position: absolute;
-    top: 9px;
-    right: 35px;
-    border-top: 1px solid var(--border-color);
-    transform: rotate(90deg)
-}
+    .switch::after {
+        content: '';
+        display: block;
+        width: 30px;
+        position: absolute;
+        top: 9px;
+        right: 35px;
+        border-top: 1px solid var(--border-color);
+        transform: rotate(90deg);
+    }
 
-.switch input {
-  display: none;
-  caret-color: transparent;
-  cursor: none;
-}
+    .switch input {
+        display: none;
+        caret-color: transparent;
+        cursor: none;
+    }
 
-.slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: var(--primary);
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-}
+    .slider {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: var(--primary);
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+    }
 
-.slider:before {
-    position: absolute;
-    content: '';
-    height: 14px;
-    width: 14px;
-    left: 3px;
-    bottom: 3px;
-    background-color: white;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-}
+    .slider:before {
+        position: absolute;
+        content: '';
+        height: 14px;
+        width: 14px;
+        left: 3px;
+        bottom: 3px;
+        background-color: white;
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+    }
 
-input:checked + .slider {
-    background-color: var(--primary);
-}
+    input:checked + .slider {
+        background-color: var(--primary);
+    }
 
-input:focus + .slider {
-    box-shadow: 0 0 1px #101010;
-}
+    input:focus + .slider {
+        box-shadow: 0 0 1px #101010;
+    }
 
-input:checked + .slider:before {
-    -webkit-transform: translateX(20px);
-    -ms-transform: translateX(20px);
-    transform: translateX(20px);
-}
+    input:checked + .slider:before {
+        -webkit-transform: translateX(20px);
+        -ms-transform: translateX(20px);
+        transform: translateX(20px);
+    }
 
-.slider.round {
-    border-radius: 34px;
-}
+    .slider.round {
+        border-radius: 34px;
+    }
 
-.slider.round:before {
-    border-radius: 50%;
-}
+    .slider.round:before {
+        border-radius: 50%;
+    }
 </style>
--- src/components/UI/MySocialBlock.vue
+++ src/components/UI/MySocialBlock.vue
@@ -1,46 +1,53 @@
 <template>
-    <a v-for='link in options' :key='link.alt' :href='link.href' class='SocialBlock' :target='link.target' :rel='link.rel'>
-        <icon-base
-            :width=35
-            :height=35
-            :icon-name='link.name'>
-        </icon-base>
+    <a
+        v-for="link in options"
+        :key="link.alt"
+        :href="link.href"
+        class="SocialBlock"
+        :target="link.target"
+        :rel="link.rel"
+    >
+        <icon-base :width="35" :height="35" :icon-name="link.name"> </icon-base>
     </a>
 </template>
 
 <script>
     export default {
         name: 'my-social-block',
-        props: {
-            options: {
-                type: Array,
-                default: () => []
-            },
+    };
+</script>
+<script setup>
+    import { defineProps } from 'vue';
+
+    const { options } = defineProps({
+        options: {
+            type: Array,
+            default: () => [],
         },
-    }
+    });
 </script>
 
 <style lang="css" scoped>
-.SocialBlock {
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 140%;
-    color: var(--text);
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 30px;
-    text-decoration: none;
-    cursor: pointer;
-    border-radius: 15px;
-    padding: 10px;
-    width: 35px;
-    height: 35px;
-}
+    .SocialBlock {
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 140%;
+        color: var(--text);
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 30px;
+        text-decoration: none;
+        cursor: pointer;
+        border-radius: 15px;
+        padding: 10px;
+        width: 35px;
+        height: 35px;
+    }
 
-.SocialBlock svg {
-    display: block;
-    object-fit: cover;
-    object-position: top;
-}
+    .SocialBlock svg {
+        display: block;
+        object-fit: cover;
+        object-position: top;
+    }
 </style>
